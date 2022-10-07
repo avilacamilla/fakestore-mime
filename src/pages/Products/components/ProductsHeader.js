9a3094a2b094a6eb65ebf2ed907a95bc8b1945bc
Camilla Avila <avilacamilla@outlook.com>
2022-10-07 07:56:49 -0300

    Update ProductHeader

--- src/pages/Products/components/ProductsHeader.js
+++ src/pages/Products/components/ProductsHeader.js
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { Add } from '@mui/icons-material';
+import Stack from '@mui/material/Stack';
+import "./ProductsHeader.css"
 
 export default (props) => {
     return (
         <div className="products-header">
-            <h1>Produtos <span className="total">25 cadastrados</span></h1>
-            <Button variant="contained" startIcon={<Add />}>
-                Adicionar Produto
-            </Button>
+            <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={2}>
+                <h1>Produtos <span className="total">25 cadastrados</span></h1>
+                <Button variant="contained" startIcon={<Add />}>
+                    Adicionar Produto
+                </Button>
+            </Stack>   
         </div>        
     );
 }
