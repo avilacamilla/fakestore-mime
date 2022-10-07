e889232ae9ef4d69a1f11abb0d418b7b760817aa
Camilla Avila <avilacamilla@outlook.com>
2022-10-07 07:47:30 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,43 @@
 import logo from "../assets/logo.jpg"
+import Stack from '@mui/material/Stack';
+import "./Header.css";
+
 
 export default () => {
     return (
         <header className="header">
-            <div className="logo">
-                <img src={ logo } />
-                <h1 className="title">
-                    Lojinha Mime
-                </h1>
+            <div className="container">
+                <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                spacing={2}>
+
+                    <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    spacing={2}>
+                        <img src={ logo } width="110px" height="110px" />
+                        <h1 className="title">
+                            Lojinha Mime
+                        </h1>
+                    </Stack>   
+                    
+                        <nav className="menu">
+                            <ul>
+                                <li><a href="#">Página inicial</a></li>
+                                <li><a href="#">Vendas</a></li>
+                                <li className="active"><a href="#">Produtos</a></li>
+                                <li><a href="#">Clientes</a></li>
+                                <li><a href="#">Marketing</a></li>
+                            </ul>
+                        </nav>
+                </Stack>
             </div>
-            <nav className="menu">
-                <ul>
-                    <li><a href="#">Página inicial</a></li>
-                    <li><a href="#">Vendas</a></li>
-                    <li><a href="#">Produtos</a></li>
-                    <li><a href="#">Clientes</a></li>
-                    <li><a href="#">Marketing</a></li>
-                </ul>
-            </nav>
+            
+           
+          
         </header>
     );
 }
